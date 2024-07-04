b5a1d1bca14a25bfcecdc3568eb3239ebcddef83
Kartik Kumar <kartikkrprajapati01@gmail.com>
2024-07-04 13:55:25 +0530

    Added body parser

--- src/index.js
+++ src/index.js
@@ -8,6 +8,22 @@ const connectDB = require('./config/DbConfig');
 
 const app = express();
 
+// app.use () -> applies body parser in application
+
+
+// to parse/read the data from input successfully
+// we also need to parse
+
+
+// for json input
+app.use(express.json());
+// for text input
+app.use(express.text());
+// browser don't know all character,
+// to encode it 
+app.use(express.urlencoded({extended:true}));
+
+
 app.listen(ServerConfig.PORT,async ()=>{
     await connectDB();
     console.log(`server started at port ${ServerConfig.PORT}...!!`);
